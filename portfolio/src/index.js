58680809d339d807ad0d40ee068a6493e782e968
yollibinter <yolandbinter330@gmail.com>
2023-05-15 18:03:43 +0100

    initial commit

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-reactDom.render(<APP/> , document.getElementById('root')); 
-/*THIS LINE INDICATE THAT
- ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE
-*/
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-reactDom.render(<APP/> , document.getElementById('root'));
- /*THIS LINE INDICATE THAT
-ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";s
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <APP />
+  </React.StrictMode>
+);
