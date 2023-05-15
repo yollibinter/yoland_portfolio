60a32c7a18ad6e99e49bc82d00750b92f088a749
GodloveDelight68 <amarachukwudelight9@gmail.com>
2023-05-15 10:22:32 +0100

    first portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from  'C:\Users\Desktop\my portfolio\portfolio\src\components\Header'
+import Navbar from './components/navbar/Navbar'
+import About from   'C:\Users\Desktop\my portfolio\portfolio\src\components\About'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from  'C:\Users\Desktop\my portfolio\portfolio\src\components\Contact'
+import Footer from  'C:\Users\Desktop\my portfolio\portfolio\src\components\Footer'
+function App() {
+return (
+<>
+<Header/>
+<Navbar/>
+<About/>
+<Experience/>
+<Services/>
+<Portfolio/>
+<Testimonial/>
+<Contact/>
+<Footer/>
+</>
+);
+}
+export default App;
---
+++ portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './header.css'
+import './CTA'
+import CTA from './CTA' //here we import the CTA component inside our header
+import me from '../../assets/changer.png'
+import HeaderSocial from './HeaderSocial' //import of headerSocial component
+export const Header = () => {
+return (
+<header>
+<div className='container header__container'>
+<h5>Hello I'm</h5>
+<h1>Amarachukwu Godlove Ahanonu</h1>
+<h5 className='text-light'>Computer Engineer </h5>
+<CTA/>//here we are using the imported CTA component
+<HeaderSocial/>//same here
+<div className='my_image'>
+<img src={me} alt='my image'/>
+</div>
+<a href='#contact' className='scroll__down'>Scroll down</a>
+</div>
+</header>
+)
+}
+export default Header;
